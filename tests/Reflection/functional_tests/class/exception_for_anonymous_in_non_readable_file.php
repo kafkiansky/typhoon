140c4c2cb4bf088cb53a3cba6a155713cb677f06
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-07-31 04:14:28 +0300

    Move Locators to Reflection\Locator, remove LocatorErrored

--- tests/Reflection/functional_tests/class/exception_for_anonymous_in_non_readable_file.php
+++ tests/Reflection/functional_tests/class/exception_for_anonymous_in_non_readable_file.php
@@ -7,13 +7,12 @@ namespace Typhoon\Reflection;
 use PHPUnit\Framework\TestCase;
 use Typhoon\DeclarationId\Id;
 use Typhoon\Reflection\Exception\FileIsNotReadable;
-use Typhoon\Reflection\Exception\LocatorErrored;
 
 return static function (TyphoonReflector $reflector, TestCase $test): void {
     $file = __FILE__ . '.fake';
     $id = Id::anonymousClass($file, 10, 20);
 
-    $test->expectExceptionObject(new LocatorErrored($id, new FileIsNotReadable($file)));
+    $test->expectExceptionObject(new FileIsNotReadable($file));
 
     $reflector->reflect($id);
 };
